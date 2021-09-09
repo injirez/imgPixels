7fe87269e0badf5db21b0164d1d4da8c0b9ad297
injirka <injirka@outlook.com>
2021-09-09 19:47:57 +0300

    Setting up for heroku

---
+++ count/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .forms import ImageForm
+from .models import Image
+import time
+from django.conf import settings
+
+import cv2
+import numpy as np
+
+
+def imageUploadView(request):
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            imgObj = form.instance
+            print(imgObj.image.url)
+            print(settings.MEDIA_ROOT)
+            img = cv2.imread(r'C:\Users\injir\Documents\imgPixels\imgPixels\media\{}'.format(imgObj.image), cv2.IMREAD_GRAYSCALE)
+            numWhite = np.sum(img == 255)
+            numBlack = np.sum(img == 0)
+            if numWhite > numBlack:
+                res = 'Number of white pixels is more then black pixels ({})'.format(numWhite)
+            elif numWhite < numBlack:
+                res = 'Number of black pixels is more then white pixels ({})'.format(numBlack)
+            elif numBlack == numWhite:
+                res = 'Number of pixels is same'
+            return render(request, 'index.html', {'form': form, 'img_obj': imgObj, 'numWhite': numWhite, 'numBlack': numBlack, 'res': res})
+    else:
+        form = ImageForm()
+    return render(request, 'index.html', {'form': form})
